e90ab4499fb1d755e39f54c58bdb4d6abb2ccac1
luquinhas10 <162441126+luquinhas10@users.noreply.github.com>
2024-09-18 11:24:18 -0300

    Add files via upload

---
+++ ativ1-ex7.py
@@ -0,0 +1,2 @@
+palavra = input("Digite uma palavra: ")
+print("Palavra invertida:", palavra[::-1])
---
+++ ativ1-ex8.py
@@ -0,0 +1,7 @@
+num = float(input("Digite um número: "))
+if num > 0:
+    print("Positivo.")
+elif num < 0:
+    print("Negativo.")
+else:
+    print("Zero.")
